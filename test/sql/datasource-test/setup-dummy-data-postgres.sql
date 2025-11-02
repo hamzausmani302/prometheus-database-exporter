0468801312bc7f2830c30e7dc9693b7cc2cdbd50
hamzausmani302 <hamza.usmani@id3.ai>
2025-11-02 20:28:24 +0400

    updated sql queries

--- test/sql/datasource-test/setup-dummy-data-postgres.sql
+++ test/sql/datasource-test/setup-dummy-data-postgres.sql
@@ -1,4 +1,3 @@
-CREATE DATABASE exporter;
 CREATE TYPE task_status AS ENUM ('pending', 'success', 'failed', 'cancelled');
 CREATE TABLE products (
     task_id SERIAL PRIMARY KEY,
@@ -6,11 +5,11 @@ CREATE TABLE products (
     task_query VARCHAR(200),
     task_name VARCHAR(100) NOT NULL,
     run_date TIMESTAMP,
-    created_date DATE DEFAULT CURRENT_DATE
+    created_date DATE DEFAULT CURRENT_DATE,
     task_status task_status NOT NULL DEFAULT 'pending'
 );
 
-INSERT INTO products (task_name,task_id_ext, task_query, run_date, task_status) VALUES ('get-failed-task', '1','select 1', '2025-03-03T00:00:00', 'pending' )
-INSERT INTO products (task_name,task_id_ext, task_query, run_date, task_status) VALUES ('get-failed-task', '2','select 2', '2025-03-04T00:00:00', 'failed' )
-INSERT INTO products (task_name,task_id_ext, task_query, run_date, task_status) VALUES ('get-failed-task', '2','select 2', '2025-03-04T00:00:00', 'cancelled' )
+INSERT INTO products (task_name,task_id_ext, task_query, run_date, task_status) VALUES ('get-failed-task', '1','select 1', '2025-03-03T00:00:00', 'pending' );
+INSERT INTO products (task_name,task_id_ext, task_query, run_date, task_status) VALUES ('get-failed-task', '2','select 2', '2025-03-04T00:00:00', 'failed' );
+INSERT INTO products (task_name,task_id_ext, task_query, run_date, task_status) VALUES ('get-failed-task', '2','select 2', '2025-03-04T00:00:00', 'cancelled' );
 
